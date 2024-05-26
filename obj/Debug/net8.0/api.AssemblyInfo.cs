0061d254fa6b00f10dc658a6fe1a2f6b1aa85153
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-26 21:50:28 +0700

    POST request

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc2144fa9f9f738de7a6e94914407c49b547c84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a886ae43a815fc631bede27bd2306877b1b8e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

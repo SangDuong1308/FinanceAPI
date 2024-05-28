bdfa55b40ede55546260ff030786590e89fc7985
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-28 17:36:38 +0700

    Posts filtering handle

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -24,9 +25,9 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] StockQueryObject query)
         {
-            var stocks = await _stockRepo.GetAllAsync();
+            var stocks = await _stockRepo.GetAllAsync(query);
 
             var stocksDto = stocks.Select(s => s.ToStockDto());
 
---
+++ Helpers/StockQueryObject.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Helpers
+{
+    public class StockQueryObject
+    {
+        public string? Symbol { get; set; } = null;
+        public string? CompanyName { get; set; } = null;
+    }
+}

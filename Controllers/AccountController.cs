e85703ccfc775039bb7ba3179dc453aeed405265
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-29 12:08:12 +0700

    JWT Authorization

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace api.Controllers
 {
@@ -12,10 +15,46 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signinManager;
+
+        // Ensure there is only one constructor
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signinManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signinManager = signinManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+                if (user == null) return Unauthorized("Invalid Username!");
+
+                var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+                if (!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+                return Ok(
+                   new NewUserDto
+                   {
+                       UserName = user.UserName,
+                       Email = user.Email,
+                       Token = _tokenService.CreateToken(user)
+                   }
+               );
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost("register")]
@@ -34,8 +73,7 @@ namespace api.Controllers
                     Email = registerDto.Email,
                 };
 
-                var createUser = await _userManager.CreateAsync(appUser,
-                    registerDto.Password);
+                var createUser = await _userManager.CreateAsync(appUser, registerDto.Password);
                 if (createUser.Succeeded)
                 {
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
@@ -45,7 +83,7 @@ namespace api.Controllers
                         {
                             UserName = appUser.UserName,
                             Email = appUser.Email,
-                            Token =_tokenService.CreateToken(appUser)
+                            Token = _tokenService.CreateToken(appUser)
                         });
                     }
                     else
@@ -57,10 +95,10 @@ namespace api.Controllers
                 {
                     return StatusCode(500, createUser.Errors);
                 }
-            } 
+            }
             catch (Exception e)
             {
-                return StatusCode(500, e);
+                return StatusCode(500, e.Message);
             }
         }
     }

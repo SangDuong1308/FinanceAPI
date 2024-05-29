0678d982768dd17a69c82656496e41dff0e7090d
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-05-29 09:57:56 +0700

    Handle user register

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc9b077e521f57d1d6a3557331f4cfe646d14d49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c498607f181cfcaceab694a94caa26b7b35e533")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
